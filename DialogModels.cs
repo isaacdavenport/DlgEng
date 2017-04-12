54ceab1fd2ccd8aa544add7a00b64cb17dbb2948
JoeRickard <joe.s.rickard@gmail.com>
2017-04-12 09:43:54 -0600

    error handling

--- DialogModels.cs
+++ DialogModels.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 using System.IO;
 
 
-namespace DialogEngine
+namespace DialogEngine  //Lets create some protections here for invalid files/entries in files. both error reporting and recovery.
 {
     public static class InitModelDialogs
     {
@@ -11,24 +11,40 @@ namespace DialogEngine
         {
             DateTime firstRound = new DateTime(2016, 5, 18);
             DateTime adventureRound = new DateTime(2016, 9, 17);
+            Boolean errorFlag = false;
 
             //Dialogs JSON parse here.
             DirectoryInfo dialogs_d = new DirectoryInfo(SessionVars.DialogsDirectory);
-            foreach (FileInfo file in dialogs_d.GetFiles("*.json")) //file of type FileInfo for each .json in directory
+            foreach (FileInfo file in dialogs_d.GetFiles("*.json")) //file of type FileInfo for each .json in directory (can be multiple ;D )
             {
-                string inDialog;
-                FileStream fs = file.OpenRead();    //open a read-only FileStream
-                using (StreamReader reader = new StreamReader(fs))   //creates new streamerader for fs stream. Could also construct with filename...
+                string inDialog;    //for input dialogs as string.
+                FileStream fs = null;
+                if (!errorFlag)
                 {
-
-                    inDialog = reader.ReadToEnd();//create string of JSON file
-                    ModelDialogInput dialogsInClass = JsonConvert.DeserializeObject< ModelDialogInput >(inDialog);  //string to Object.
-                    foreach(ModelDialog curDialog in dialogsInClass.inList)
+                    try
+                    { fs = file.OpenRead(); }    //open a read-only FileStream
+                    catch (IOException e)
                     {
-                        //Add to dialog List
-                        inObj.ModelDialogs.Add(curDialog);
-                        //pop sum
-                        inObj.DialogModelPopularitySum += curDialog.Popularity;
+                        errorFlag = true;
+                        Console.WriteLine("ioexception thrown. likely that another thread has the file open already. " + e.GetType().Name);
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine(e.Data);
+                    }
+                }
+                if (!errorFlag)
+                {
+                    using (StreamReader reader = new StreamReader(fs))   //creates new streamerader for fs stream. Could also construct with filename...
+                    {
+
+                        inDialog = reader.ReadToEnd();//create string of JSON file
+                        ModelDialogInput dialogsInClass = JsonConvert.DeserializeObject<ModelDialogInput>(inDialog);  //string to Object.
+                        foreach (ModelDialog curDialog in dialogsInClass.inList)
+                        {
+                            //Add to dialog List
+                            inObj.ModelDialogs.Add(curDialog);
+                            //popularity sum 
+                            inObj.DialogModelPopularitySum += curDialog.Popularity;
+                        }
                     }
                 }
             }
