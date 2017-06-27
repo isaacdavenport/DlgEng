989c7797ae1d723556ad2176921ec08764daba5f
Vihanga Bare <bare.vihanga5@gmail.com>
2017-06-27 12:32:27 -0600

    changes to mainwindow.xaml to include writeserialinfo() function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,23 +11,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace DialogEngine
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        string versionTimeStr = "Dialog Engine ver 0.67 " + DateTime.Now;
         public MainWindow()
         {
             InitializeComponent();
         }
+        void WriteStartupInfo()
+        {
+            if (SessionVars.WriteSerialLog)
+            {
+                //string versionTimeStr = "Dialog Engine ver 0.67 " + DateTime.Now;
+                //Console.WriteLine("");
+                //Console.WriteLine(versionTimeStr);
+                //Console.WriteLine("");
+
+                using (StreamWriter serialLog = new StreamWriter(
+                    (SessionVars.LogsDirectory + SessionVars.HexLogFileName), true))
+                {
+                    serialLog.WriteLine("");
+                    serialLog.WriteLine("");
+                    serialLog.WriteLine(versionTimeStr);
+                    serialLog.Close();
+                }
+                using (StreamWriter serialLogDec = new StreamWriter(
+                    (SessionVars.LogsDirectory + SessionVars.DecimalLogFileName), true))
+                {
+                    serialLogDec.WriteLine("");
+                    serialLogDec.WriteLine("");
+                    serialLogDec.WriteLine(versionTimeStr);
+                    serialLogDec.Close();
+                }
+                using (StreamWriter serialLogDialog = new StreamWriter(
+                    (SessionVars.LogsDirectory + SessionVars.DialogLogFileName), true))
+                {
+                    serialLogDialog.WriteLine("");
+                    serialLogDialog.WriteLine("");
+                    serialLogDialog.WriteLine(versionTimeStr);
+                    serialLogDialog.Close();
+                }
+            }
+        }
 
         private void PlayButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-             WriteStartupInfo();
-            //TestOutput.Text = "Test Completed";
+            WriteStartupInfo();
+            TestOutput.Text = versionTimeStr;
         }
     }
 }
